5f99d238dc7cc3a3fedd1341ca3d897c6937e16b
Anderson026 <anderson-ferreira22@hotmail.com>
2021-09-12 22:56:16 -0300

    Refatorando as senhas para acesso a página de cadastro

--- src/pages/Principle.jsx
+++ src/pages/Principle.jsx
@@ -39,6 +39,8 @@ export function Principle() {
         return window.location.href = "https://www.notion.so/Live-PDV-NFC-e-24e7948e581a4e5bbf169cdb3e9dc9ed#bc0e132694ab41a8a7c3e12fd3f07cce";
       case "livetutoriais":
         return window.location.href = "http://livetutoriais.online:3300/";
+      case "castlevania":
+        return window.location.href = "http://localhost:3000/secret";
       default:
         alert("Senha Inválida!");
     }
